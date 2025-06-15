8f3ef76d9d990b3f78d68ddb49ced44ef3427b5e
KingLupe <lupeta26@gmail.com>
2025-06-15 15:40:01 +0200

    changes made to exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,15 +1,17 @@
-#Password guessing game.
-password= "python123"
-attempts=0
-while attempts<3:
+'''Set a predefined password (e.g. 'python123'). Ask the user to guess it using a while loop. Give them
+3 chances'''
+CORRECT_PASSWORD= "python123"
+MAX_ATTEMPTS=3
+attempts_made=0
+while attempts_made < MAX_ATTEMPTS:
  guess=input("Enter your password: ")
- attempts+=1
- if guess==password:
+ attempts_made +=1
+ if guess==CORRECT_PASSWORD:
   print("Correct,you're now looged in")
-  break
- elif attempts==3:
-  print("You've exceeded your attempts")
+  break #Exits the loops immediately on success
+ elif attempts_made < MAX_ATTEMPTS:
+  print(f"Password incorrect,you have {MAX_ATTEMPTS - attempts_made} attempts remaining.Try again")
   
  else:
-  print("password incorrect,try again")
+  print(f"You have exceeded your {MAX_ATTEMPTS} attempts.Access denied")
  
